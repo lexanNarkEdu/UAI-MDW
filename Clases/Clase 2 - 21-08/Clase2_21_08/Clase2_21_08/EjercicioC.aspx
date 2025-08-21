6f546378c140412314a31a41b5246089ad902648
lexanNarkEdu <149331719+lexanNarkEdu@users.noreply.github.com>
2025-08-21 20:24:58 -0300

    Se agrega ejercicio C, que simula un carrito de comrpas

---
+++ Clases/Clase 2 - 21-08/Clase2_21_08/Clase2_21_08/EjercicioC.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="EjercicioC.aspx.cs" Inherits="Clase2_21_08.EjercicioC" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h3>DOM: Elija un producto</h3>
+            <br />
+            <div>
+                <label>Producto: </label>
+                <asp:DropDownList ID="ddlProducto" runat="server"></asp:DropDownList>
+                <label style="margin-left:20px">Cantidad: </label>
+                <asp:TextBox ID="txtCantidad" runat="server"></asp:TextBox>
+            </div>
+            <br />
+            
+            <asp:Button ID="btnEnviar" runat="server" Text="Enviar" OnClick="btnEnviar_Click" />
+            <div>
+                <label>Precio: </label>
+                <asp:Label ID="lblPrecio" runat="server"></asp:Label> pta
+            </div>
+            <div>
+                <label>Total: </label>
+                <asp:Label ID="lblTotal" runat="server"></asp:Label> pta
+            </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Clases/Clase 2 - 21-08/Clase2_21_08/Clase2_21_08/EjercicioC.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+
+namespace Clase2_21_08
+{
+    public partial class EjercicioC : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                XmlDocument miDoc = new XmlDocument();
+                XmlTextReader miLector = new XmlTextReader(Server.MapPath("inventario2.xml"));
+                miLector.WhitespaceHandling = WhitespaceHandling.None;
+                miDoc.Load(miLector);
+                Session.Add("DocumentoEnSession", miDoc);
+
+                for (int i = 0; i < miDoc.DocumentElement.ChildNodes.Count; i++)
+                {
+                    ddlProducto.Items.Add(miDoc.DocumentElement.ChildNodes[i].ChildNodes[1].InnerText);
+                }
+                miLector.Close();
+            }
+        }
+
+        protected void btnEnviar_Click(object sender, EventArgs e)
+        {
+            int n;
+            int cantidad = 1;
+            double precio;
+            XmlDocument miDoc;
+            miDoc = (XmlDocument)Session["DocumentoEnSession"];
+            n = ddlProducto.SelectedIndex;
+            cantidad = Convert.ToInt32(txtCantidad.Text);
+
+            precio = Convert.ToDouble(miDoc.DocumentElement.ChildNodes[n].ChildNodes[2].InnerText);
+
+            lblPrecio.Text = precio.ToString();
+            lblTotal.Text = (cantidad * precio).ToString();
+        }
+    }
+}
