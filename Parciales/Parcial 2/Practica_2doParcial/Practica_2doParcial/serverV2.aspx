11170912ecd7be84c764007bc28a684ea6e57c10
lexanNarkEdu <149331719+lexanNarkEdu@users.noreply.github.com>
2025-11-06 17:12:22 -0300

    Practica Ejercicio Parcial 2, parte 2 (otra forma mas simple)

---
+++ Parciales/Parcial 2/Practica_2doParcial/Practica_2doParcial/serverV2.aspx
@@ -0,0 +1,15 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="serverV2.aspx.cs" Inherits="Practica_2doParcial.serverV2" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Parciales/Parcial 2/Practica_2doParcial/Practica_2doParcial/serverV2.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Linq;
+using System.Xml.Xsl;
+
+namespace Practica_2doParcial
+{
+    public partial class serverV2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Append2Professors();
+            ApplySalayPerTwoWithXslt();
+        }
+
+        private void Append2Professors()
+        {
+            // Cargar el XML
+            XDocument doc = XDocument.Load(Server.MapPath("Profesores.xml"));
+
+            // Lista de profesores
+            List<Profesor> listaProfesores = new List<Profesor>()
+            {
+                new Profesor() { Apellido = "Gomez", Sueldo = 1_500_000, Condicion = "Titular" },
+                new Profesor() { Apellido = "Fernandez", Sueldo = 1_000_000, Condicion = "Adjunto" }
+            };
+
+            // Agregar profesores de forma simple
+            foreach (var p in listaProfesores)
+            {
+                doc.Root.Add(
+                    new XElement("Profesor",
+                        new XElement("Apellido", p.Apellido),
+                        new XElement("Sueldo", p.Sueldo),
+                        new XElement("Condicion", p.Condicion)
+                    )
+                );
+            }
+
+            // Guardar
+            doc.Save(Server.MapPath("Profesores.xml"));
+        }
+
+        public void ApplySalayPerTwoWithXslt() 
+        {
+            XslCompiledTransform xslt = new XslCompiledTransform();
+            xslt.Load(Server.MapPath("DuplicarSueldoTitulares.xslt"));
+
+            xslt.Transform(
+                Server.MapPath("Profesores.xml"),
+                Server.MapPath("ProfesoresTransformados.xml")
+            );
+        }
+    }
+}
